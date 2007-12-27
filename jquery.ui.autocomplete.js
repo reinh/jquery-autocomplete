2010208d688925f65b2ca4d6eb8f8af2565ce459
Rein Henrichs <reinh@reinh.com>
2007-12-27 15:32:01 -0600

    Initial creation of jquery.ui.autocomplete.js
    Signed-off-by: Rein Henrichs <reinh@reinh.com>

---
+++ jquery.ui.autocomplete.js
@@ -0,0 +1,9 @@
+/*
+ * UI Autocomplete - jQuery plugin 0.0.1
+ *
+ * Copyright (c) 2007 Yehuda Katz, Rein Henrichs
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
