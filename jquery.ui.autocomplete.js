ce015c9b3986a33b434ffd5b7355f7037dfdd0d8
Yehuda Katz <wycats@gmail.com>
2007-12-27 18:39:53 -0800

    and it looks good

--- jquery.ui.autocomplete.js
+++ jquery.ui.autocomplete.js
@@ -18,15 +18,15 @@
     var selected = -1;
     
     input
-      .blur(function() { container.remove(); $("body").unbind("keydown.autocomplete"); })
+      .blur(function() { container.remove(); })
       .keydown(function(e) {
         if(e.which == 27) { 
           input.val(original); 
           container.remove();
-          return false;
+          $.data(input[0], "suppressKey", true);
         } else if(e.which == 13) { 
           container.remove(); 
-          return false;
+          $.data(input[0], "suppressKey", true);
         } else if(e.which == 40 || e.which == 9 || e.which == 38) {
           switch(e.which) {
             case 40: 
@@ -35,15 +35,11 @@
             case 38:
               selected = selected <= 0 ? size - 1 : selected - 1;
           }
-          console.log(selected);
           var active = $("> *", container).removeClass("active")
             .slice(selected, selected + 1).addClass("active");
           input.val(active.html());
         }
-        if(e.which == 9) return false;        
-      })
-      .keypress(function() {
-        console.log("Hello");
+        if(e.which == 9) $.data(input[0], "suppressKey", true);
       });
   };
   
@@ -55,7 +51,6 @@
       template: function(str) { return "<li>" + str + "</li>"; },
       match: function(typed) { return !!this.match(new RegExp(typed)); },
       wrapper: "<ul class='jq-ui-autocomplete'></ul>",
-      emptyList: "None"
     }, opt);
   
     return this.each(function() {
@@ -65,6 +60,7 @@
           var typingTimeout = $.data(this, "typingTimeout");
           if(typingTimeout) window.clearInterval(typingTimeout);
           if($.data(this, "justCanceled")) $.data(this, "justCanceled", false);
+          else if($.data(this, "suppressKey")) { return $.data(this, "suppressKey", false); }
           else {
             $.data(this, "typingTimeout", setTimeout(function() { 
               $(e.target).trigger("autocomplete"); 
@@ -76,9 +72,10 @@
           var list = $(opt.getList())
             .filter(function() { return opt.match.call(this, self.val()); })
             .map(function() { return opt.template(this); }).get();
-            
-          var container = $(list.join("") || opt.template(opt.emptyList))
-            .wrapAll(opt.wrapper).parents(":last").children();
+          
+          if(!list.length) return false;
+          
+          var container = $(list.join("")).wrapAll(opt.wrapper).parents(":last").children();
             
           var offset = self.offset();
           if(opt.container) opt.container.remove();
