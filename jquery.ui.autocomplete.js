b87be68fc3c74c43e916ef9a34704079ae01f1c0
Yehuda Katz <wycats@gmail.com>
2007-12-27 18:29:58 -0800

    sooo close

--- jquery.ui.autocomplete.js
+++ jquery.ui.autocomplete.js
@@ -13,6 +13,40 @@
   $.ui = $.ui || {};
   $.ui.autocomplete = {};
   
+  $.fn.autocompleteMode = function(container, input, size) {
+    var original = input.val();
+    var selected = -1;
+    
+    input
+      .blur(function() { container.remove(); $("body").unbind("keydown.autocomplete"); })
+      .keydown(function(e) {
+        if(e.which == 27) { 
+          input.val(original); 
+          container.remove();
+          return false;
+        } else if(e.which == 13) { 
+          container.remove(); 
+          return false;
+        } else if(e.which == 40 || e.which == 9 || e.which == 38) {
+          switch(e.which) {
+            case 40: 
+            case 9:
+              selected = selected >= size - 1 ? 0 : selected + 1; break;
+            case 38:
+              selected = selected <= 0 ? size - 1 : selected - 1;
+          }
+          console.log(selected);
+          var active = $("> *", container).removeClass("active")
+            .slice(selected, selected + 1).addClass("active");
+          input.val(active.html());
+        }
+        if(e.which == 9) return false;        
+      })
+      .keypress(function() {
+        console.log("Hello");
+      });
+  };
+  
   $.fn.autocomplete = function(opt) {
     
     opt = $.extend({}, {
@@ -30,17 +64,20 @@
         .keypress(function(e) {
           var typingTimeout = $.data(this, "typingTimeout");
           if(typingTimeout) window.clearInterval(typingTimeout);
-          $.data(this, "typingTimeout", setTimeout(function() { 
-            $(e.target).trigger("autocomplete"); 
-          }, opt.timeout));
+          if($.data(this, "justCanceled")) $.data(this, "justCanceled", false);
+          else {
+            $.data(this, "typingTimeout", setTimeout(function() { 
+              $(e.target).trigger("autocomplete"); 
+            }, opt.timeout));
+          }
         })
         .bind("autocomplete", function() {
           var self = $(this);
           var list = $(opt.getList())
             .filter(function() { return opt.match.call(this, self.val()); })
-            .map(function() { return opt.template(this); }).get().join("");
+            .map(function() { return opt.template(this); }).get();
             
-          var container = $(list || opt.template(opt.emptyList))
+          var container = $(list.join("") || opt.template(opt.emptyList))
             .wrapAll(opt.wrapper).parents(":last").children();
             
           var offset = self.offset();
@@ -51,6 +88,8 @@
             left: offset.left,
             width: self.width()
           }).appendTo("body");
+          
+          $("body").autocompleteMode(container, self, list.length);
         });
 
     });
