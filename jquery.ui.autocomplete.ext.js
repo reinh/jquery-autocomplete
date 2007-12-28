af39fb71b5ac6fb5a61cb72e8b00bb33f30a0942
Yehuda Katz <wycats@gmail.com>
2007-12-27 21:10:00 -0800

    support for objects

---
+++ jquery.ui.autocomplete.ext.js
@@ -0,0 +1,23 @@
+/*
+ * UI Autocomplete - jQuery plugin 0.0.1
+ *
+ * Copyright (c) 2007 Yehuda Katz, Rein Henrichs
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+(function($) {
+  $.ui = $.ui || {};
+  $.ui.autocomplete = $.ui.autocomplete || {};
+  $.ui.autocomplete.ext = $.ui.autocomplete.ext || {};
+  
+  $.ui.autocomplete.ext.ajax = function(opt) {
+    var ajax = opt.ajax;
+    return { getList: function(input) { 
+      $.getJSON(ajax, "val=" + input.val(), function(json) { input.trigger("updateList", [json]); }); 
+    } };
+  };
+  
+})(jQuery);
