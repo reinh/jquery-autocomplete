9f3dee1c07c11e292024841fa9d5c7caec4d24cd
gimenete <gimenete@gmail.com>
2009-06-19 04:20:25 -0700

    Better to use a dictionary {} than to concatenate "val=" with the input value.

--- jquery.ui.autocomplete.ext.js
+++ jquery.ui.autocomplete.ext.js
@@ -19,7 +19,7 @@
     var ajax = opt.ajax;
     return { getList: function(input) { 
 			if (input.val().match(/^\s*$/)) return false;
-      $.getJSON(ajax, "val=" + input.val(), function(json) { input.trigger("updateList", [json]); }); 
+      $.getJSON(ajax, { val: input.val() }, function(json) { input.trigger("updateList", [json]); });
     } };
   };
   
