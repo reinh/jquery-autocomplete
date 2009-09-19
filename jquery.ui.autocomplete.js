14d2ae0d4dad9b0c3a1b466fbc391b95b66ee1f3
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2009-09-18 19:32:23 -0700

    Made updateList and dismissList overridable (provide in options).

--- jquery.ui.autocomplete.js
+++ jquery.ui.autocomplete.js
@@ -41,6 +41,27 @@
       timeout: 500,
       threshold: 100,
       getList: function(input) { input.trigger("updateList", [opt.list]); },
+      updateList: function(list, val) {
+        list = $(list)
+          .filter(function() { return opt.match.call(this, val); })
+          .map(function() {
+            var node = $(opt.template(this))[0];
+            $.data(node, "originalObject", this);
+            return node;
+          });
+
+        if(!list.length || list.length > opt.threshold) { return false; }
+
+        var container = list.wrapAll(opt.wrapper).parents(":last").children();
+        // IE seems to wrap the wrapper in a random div wrapper so
+        // drill down to the node in opt.wrapper.
+        var wrapper_tagName = $(opt.wrapper)[0].tagName;
+        for (;container[0].tagName !== wrapper_tagName; container = container.children(':first')) {}
+        return container;
+      },
+      dismissList: function(container) {
+        container.remove();
+      },
       template: function(str) { return "<li>" + opt.insertText(str) + "</li>"; },
       insertText: function(str) { return str; },
       match: function(typed) { return this.match(new RegExp(typed)); },
@@ -83,8 +104,7 @@
               (k == KEY.UP || k == KEY.DOWN) &&
               !$.data(this, "typingTimeout")) {
             startTypingTimeout(e, this);
-          }
-          else {
+          } else {
             preventTabInAutocompleteMode(e);
           }
         })
@@ -93,42 +113,31 @@
           var k = e.keyCode || e.which; // keyCode == 0 in Gecko/FF on keypress
           if(typingTimeout) window.clearInterval(typingTimeout);
 
-          if($.data(document.body, "suppressKey")) return $.data(document.body, "suppressKey", false);
-          else if($.data(document.body, "autocompleteMode") && k < 32 && k != KEY.BS && k != KEY.DEL) return false;
-          else if (k == KEY.BS || k == KEY.DEL || k > 32) { // more than ESC and RETURN and the like
+          if($.data(document.body, "suppressKey")) {
+            return $.data(document.body, "suppressKey", false);
+          } else if($.data(document.body, "autocompleteMode") && k < 32 && k != KEY.BS && k != KEY.DEL) {
+            return false;
+          } else if (k == KEY.BS || k == KEY.DEL || k > 32) { // more than ESC and RETURN and the like
             startTypingTimeout(e, this);
           }
         })
         .bind("autocomplete", function() {
-          var self = $(this),
-              val = self.val();
+          var self = $(this);
 
           self.one("updateList", function(e, list) {
-            list = $(list)
-              .filter(function() { return opt.match.call(this, val); })
-              .map(function() {
-                var node = $(opt.template(this))[0];
-                $.data(node, "originalObject", this);
-                return node;
-              });
+            var listContainer = opt.updateList(list, self.val());
+            if (listContainer === false) return false;
+            list = listContainer.find("li");
 
             $("body").trigger("off.autocomplete");
-
             if(!list.length || list.length > opt.threshold) return false;
 
-            var container = list.wrapAll(opt.wrapper).parents(":last").children();
-            // IE seems to wrap the wrapper in a random div wrapper so
-            // drill down to the node in opt.wrapper.
-            var wrapper_tagName = $(opt.wrapper)[0].tagName;
-            for (;container[0].tagName !== wrapper_tagName; container = container.children(':first')) {}
-
             var offset = self.offset();
-
-            opt.container = container
+            opt.container = listContainer
               .css({top: offset.top + self.outerHeight(), left: offset.left, width: self.width()})
               .appendTo("body");
 
-            $("body").autocompleteMode(container, self, list.length, opt);
+            $("body").autocompleteMode(listContainer, self, list.length, opt);
           });
 
           opt.getList(self);
